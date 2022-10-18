630f2e63788a398de5ec298643f145faa47dd4f2
Ifeanyi <ifeanyio569@outlook.com>
2022-10-18 11:16:03 +0100

    Update PYDER (MAIN).py

--- PYDER (MAIN).py
+++ PYDER (MAIN).py
@@ -1,277 +1,217 @@
-print('Ifeanyi Virtual assistant robot v2.7.9\n\n')
-#name input
-Name = input("What is your Name: ")
-username = input("Please enter a username:")
-password = input("Please enter a password:")
-#login
-username1 = 'username' 
-password_user = 'password'
+import pyttsx3
+import webbrowser
+print("© Copyright 2022 Ifeanyi Onuama. All rights reserved")
+def speak(audio):
+	
+	engine = pyttsx3.init()
+	# getter method(gets the current value
+	# of engine property)
+	voices = engine.getProperty('voices')
+	
+	# setter method .[0]=male voice and
+	# [1]=female voice in set Property.
+	engine.setProperty('voice', voices[1].id)
+	
+	# Method for the speaking of the assistant
+	engine.say(audio)
+	
+	# Blocks while processing all the currently
+	# queued commands
+	engine.runAndWait()
+speak("There is a newer version of this application: would you like to open?")
+open = input("(yes/no): ")
+if open == 'yes':
+    webbrowser.open("www.github.com/pycodeapps/pyder-2.0")
+    speak("run program again to continue")
+    exit()
+else:
+    speak("Welcome to Pieder 1.0, what would you like to do?")
+todo = input("Game(1)\nYoutube(2)\nGoogle(3)\nGoodbye(4):\n")
 
-userInput = input("What is your username?\n")
-if userInput == username1:
-    a=input("Password?\n")
-    if a == 'password_user':
-        print("Correct password, Welcome "+Name+"")
-     #activity area
-activity_user = input("\nWhat would you like to do:\nGames\nCalculator\nGoogle\nSettings\nYoutube:\n")
-#settings is being worked on
-if activity_user == 'Games':
-  print("Loading Games...")
-  game=input("Snake(1):\nEgg Catcher(2):\n")
-  if game=='1':
-      import turtle
-import time
-import random
 
-delay = 0.1
-score = 0
-high_score = 0
+def snake():
+        import time
+        import turtle
+        import random
+
+        delay = 0.1
+        score = 0
+        high_score = 0
 
 
 
-# Creating a window screen
-wn = turtle.Screen()
-wn.title("Ifeanyi PYDER Snake Game")
-wn.bgcolor("black")
-# the width and height can be put as user's choice
-wn.setup(width=600, height=600)
-wn.tracer(0)
+        # Creating a window screen
+        wn = turtle.Screen()
+        wn.title("Ifeanyi PYDER Snake Game")
+        wn.bgcolor("black")
+        # the width and height can be put as user's choice
+        wn.setup(width=600, height=600)
+        wn.tracer(0)
 
 # head of the snake
-head = turtle.Turtle()
-head.shape("square")
-head.color("white")
-head.penup()
-head.goto(0, 0)
-head.direction = "Stop"
+        head = turtle.Turtle()
+        head.shape("square")
+        head.color("white")
+        head.penup()
+        head.goto(0, 0)
+        head.direction = "Stop"
 
 # food in the game
-food = turtle.Turtle()
-colors = random.choice(['red', 'green', 'black'])
-shapes = random.choice(['square', 'triangle', 'circle'])
-food.speed(0)
-food.shape(shapes)
-food.color(colors)
-food.penup()
-food.goto(0, 100)
+        food = turtle.Turtle()
+        colors = random.choice(['red', 'green', 'black'])
+        shapes = random.choice(['square', 'triangle', 'circle'])
+        food.speed(0)
+        food.shape(shapes)
+        food.color(colors)
+        food.penup()
+        food.goto(0, 100)
 
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 250)
-pen.write("Score : 0 High Score : 0", align="center",
+        pen = turtle.Turtle()
+        pen.speed(0)
+        pen.shape("square")
+        pen.color("white")
+        pen.penup()
+        pen.hideturtle()
+        pen.goto(0, 250)
+        pen.write("Score : 0 High Score : 0", align="center",
 		font=("candara", 24, "bold"))
 
 
 
 # assigning key directions
-def group():
-	if head.direction != "down":
-		head.direction = "up"
+        def group():
+                if head.direction != "down":
+                        head.direction = "up"
 
 
-def godown():
-	if head.direction != "up":
-		head.direction = "down"
+        def godown():
+                if head.direction != "up":
+                        head.direction = "down"
 
 
-def goleft():
-	if head.direction != "right":
-		head.direction = "left"
+        def goleft():
+                if head.direction != "right":
+                        head.direction = "left"
 
 
-def goright():
-	if head.direction != "left":
-		head.direction = "right"
+        def goright():
+                if head.direction != "left":
+                        head.direction = "right"
 
 
-def move():
-	if head.direction == "up":
-		y = head.ycor()
-		head.sety(y+20)
-	if head.direction == "down":
-		y = head.ycor()
-		head.sety(y-20)
-	if head.direction == "left":
-		x = head.xcor()
-		head.setx(x-20)
-	if head.direction == "right":
-		x = head.xcor()
-		head.setx(x+20)
+        def move():
+                if head.direction == "up":
+                        y = head.ycor()
+                        head.sety(y+20)
+                if head.direction == "down":
+                        y = head.ycor()
+                        head.sety(y-20)
+                if head.direction == "left":
+                        x = head.xcor()
+                        head.setx(x-20)
+                if head.direction == "right":
+                        x = head.xcor()
+                        head.setx(x+20)
 
 
 		
-wn.listen()
-wn.onkeypress(group, "w")
-wn.onkeypress(godown, "s")
-wn.onkeypress(goleft, "a")
-wn.onkeypress(goright, "d")
+        wn.listen()
+        wn.onkeypress(group, "w")
+        wn.onkeypress(godown, "s")
+        wn.onkeypress(goleft, "a")
+        wn.onkeypress(goright, "d")
 
-segments = []
-
-
-
-# Main Gameplay
-while True:
-	wn.update()
-	if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
-		time.sleep(1)
-		head.goto(0, 0)
-		head.direction = "Stop"
-		colors = random.choice(['red', 'blue', 'green'])
-		shapes = random.choice(['square', 'circle'])
-		for segment in segments:
-			segment.goto(1000, 1000)
-		segments.clear()
-		score = 0
-		delay = 0.1
-		pen.clear()
-		pen.write("Score : {} High Score : {} ".format(
-			score, high_score), align="center", font=("candara", 24, "bold"))
-	if head.distance(food) < 20:
-		x = random.randint(-270, 270)
-		y = random.randint(-270, 270)
-		food.goto(x, y)
-
-		# Adding segment
-		new_segment = turtle.Turtle()
-		new_segment.speed(0)
-		new_segment.shape("square")
-		new_segment.color("orange") # tail colour
-		new_segment.penup()
-		segments.append(new_segment)
-		delay -= 0.001
-		score += 10
-		if score > high_score:
-			high_score = score
-		pen.clear()
-		pen.write("Score : {} High Score : {} ".format(
-			score, high_score), align="center", font=("candara", 24, "bold"))
-	# Checking for head collisions with body segments
-	for index in range(len(segments)-1, 0, -1):
-		x = segments[index-1].xcor()
-		y = segments[index-1].ycor()
-		segments[index].goto(x, y)
-	if len(segments) > 0:
-		x = head.xcor()
-		y = head.ycor()
-		segments[0].goto(x, y)
-	move()
-	for segment in segments:
-		if segment.distance(head) < 20:
-			time.sleep(1)
-			head.goto(0, 0)
-			head.direction = "stop"
-			colors = random.choice(['red', 'blue', 'green'])
-			shapes = random.choice(['square', 'circle'])
-			for segment in segments:
-				segment.goto(1000, 1000)
-			segment.clear()
-
-			score = 0
-			delay = 0.1
-			pen.clear()
-			pen.write("Score : {} High Score : {} ".format(
-				score, high_score), align="center", font=("candara", 24, "bold"))
-	time.sleep(delay)
-
-wn.mainloop()
-      
-
-if activity_user == 'Calculator':
-    import tkinter as tk
-import tkinter.messagebox
-from tkinter.constants import SUNKEN
-
-window = tk.Tk()
-window.title('PYDER Calculator')
-frame = tk.Frame(master=window, bg="skyblue", padx=10)
-frame.pack()
-entry = tk.Entry(master=frame, relief=SUNKEN, borderwidth=3, width=30)
-entry.grid(row=0, column=0, columnspan=3, ipady=2, pady=2)
+        segments = []
 
 
-def myclick(number):
- entry.insert(tk.END, number)
 
+        # Main Gameplay
+        while True:
+                wn.update()
+                if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+                        time.sleep(1)
+                        head.goto(0, 0)
+                        head.direction = "Stop"
+                        colors = random.choice(['red', 'blue', 'green'])
+                        shapes = random.choice(['square', 'circle'])
+                        for segment in segments:
+                                segment.goto(1000, 1000)
+                        segments.clear()
+                        score = 0
+                        delay = 0.1
+                        pen.clear()
+                        pen.write("Score : {} High Score : {} ".format(
+                                score, high_score), align="center", font=("candara", 24, "bold"))
+                if head.distance(food) < 20:
+                        x = random.randint(-270, 270)
+                        y = random.randint(-270, 270)
+                        food.goto(x, y)
 
-def equal():
- try:
-  y = str(eval(entry.get()))
-  entry.delete(0, tk.END)
-  entry.insert(0, y)
- except:
-  tkinter.messagebox.showinfo("Error", "Syntax Error")
+                        # Adding segment
+                        new_segment = turtle.Turtle()
+                        new_segment.speed(0)
+                        new_segment.shape("square")
+                        new_segment.color("orange") # tail colour
+                        new_segment.penup()
+                        segments.append(new_segment)
+                        delay -= 0.001
+                        score += 10
+                        if score > high_score:
+                                high_score = score
+                        pen.clear()
+                        pen.write("Score : {} High Score : {} ".format(
+                                score, high_score), align="center", font=("candara", 24, "bold"))
+                # Checking for head collisions with body segments
+                for index in range(len(segments)-1, 0, -1):
+                        x = segments[index-1].xcor()
+                        y = segments[index-1].ycor()
+                        segments[index].goto(x, y)
+                if len(segments) > 0:
+                        x = head.xcor()
+                        y = head.ycor()
+                        segments[0].goto(x, y)
+                move()
+                for segment in segments:
+                        if segment.distance(head) < 20:
+                                time.sleep(1)
+                                head.goto(0, 0)
+                                head.direction = "stop"
+                                colors = random.choice(['red', 'blue', 'green'])
+                                shapes = random.choice(['square', 'circle'])
+                                for segment in segments:
+                                        segment.goto(1000, 1000)
+                                segment.clear()
 
+                                score = 0
+                                delay = 0.1
+                                pen.clear()
+                                pen.write("Score : {} High Score : {} ".format(
+                                        score, high_score), align="center", font=("candara", 24, "bold"))
+                time.sleep(delay)
 
-def clear():
- entry.delete(0, tk.END)
+        wn.mainloop()
+              
 
+    
+def goodbye():
+    speak("bye bye")
 
-button_1 = tk.Button(master=frame, text='1', padx=15,
-     pady=5, width=3, command=lambda: myclick(1))
-button_1.grid(row=1, column=0, pady=2)
-button_2 = tk.Button(master=frame, text='2', padx=15,
-     pady=5, width=3, command=lambda: myclick(2))
-button_2.grid(row=1, column=1, pady=2)
-button_3 = tk.Button(master=frame, text='3', padx=15,
-     pady=5, width=3, command=lambda: myclick(3))
-button_3.grid(row=1, column=2, pady=2)
-button_4 = tk.Button(master=frame, text='4', padx=15,
-     pady=5, width=3, command=lambda: myclick(4))
-button_4.grid(row=2, column=0, pady=2)
-button_5 = tk.Button(master=frame, text='5', padx=15,
-     pady=5, width=3, command=lambda: myclick(5))
-button_5.grid(row=2, column=1, pady=2)
-button_6 = tk.Button(master=frame, text='6', padx=15,
-     pady=5, width=3, command=lambda: myclick(6))
-button_6.grid(row=2, column=2, pady=2)
-button_7 = tk.Button(master=frame, text='7', padx=15,
-     pady=5, width=3, command=lambda: myclick(7))
-button_7.grid(row=3, column=0, pady=2)
-button_8 = tk.Button(master=frame, text='8', padx=15,
-     pady=5, width=3, command=lambda: myclick(8))
-button_8.grid(row=3, column=1, pady=2)
-button_9 = tk.Button(master=frame, text='9', padx=15,
-     pady=5, width=3, command=lambda: myclick(9))
-button_9.grid(row=3, column=2, pady=2)
-button_0 = tk.Button(master=frame, text='0', padx=15,
-     pady=5, width=3, command=lambda: myclick(0))
-button_0.grid(row=4, column=1, pady=2)
+def google():
+    import pywhatkit as kt
+    query = input("Search here: ")
+    kt.search(query)
 
-button_add = tk.Button(master=frame, text="+", padx=15,
-     pady=5, width=3, command=lambda: myclick('+'))
-button_add.grid(row=5, column=0, pady=2)
+def youtube():
+    webbrowser.opne("www.youtube.com")
 
-button_subtract = tk.Button(
- master=frame, text="-", padx=15, pady=5, width=3, command=lambda: myclick('-'))
-button_subtract.grid(row=5, column=1, pady=2)
+if todo == '1':
+    snake()
 
-button_multiply = tk.Button(
- master=frame, text="*", padx=15, pady=5, width=3, command=lambda: myclick('*'))
-button_multiply.grid(row=5, column=2, pady=2)
+if todo == '3':
+    google()
 
-button_div = tk.Button(master=frame, text="/", padx=15,
-     pady=5, width=3, command=lambda: myclick('/'))
-button_div.grid(row=6, column=0, pady=2)
+if todo == '4':
+    goodbye()
 
-button_clear = tk.Button(master=frame, text="clear",
-      padx=15, pady=5, width=12, command=clear)
-button_clear.grid(row=6, column=1, columnspan=2, pady=2)
-
-button_equal = tk.Button(master=frame, text="=", padx=15,
-      pady=5, width=9, command=equal)
-button_equal.grid(row=7, column=0, columnspan=3, pady=2)
-
-window.mainloop()
-
- 
-if activity_user == 'Google':
-  print("Search here: ")
-
-if activity_user == 'Youtube':
-  print("Search Here")
-
+if todo == '2':
+    youtube()
